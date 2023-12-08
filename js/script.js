407bb86bcf7d6256dedab4db5d90036e53bf5e7c
daATva <riven1231@mail.ru>
2023-12-08 23:49:56 +0400

    task

--- js/script.js
+++ js/script.js
@@ -1060,21 +1060,46 @@
 
 // sumArray(check)
 
-function printSum(sum) {
-    sum = 0 
-    for(let key of numbers){
-        sum += key
-    }
-    console.log("Сумма массива:", sum);
-}
+// function printSum(sum) {
+//     sum = 0 
+//     for(let key of numbers){
+//         sum += key
+//     }
+//     console.log("Сумма массива:", sum);
+// }
   
-const numbers = [1 , 3 ,4 ,5 , 23]
+// const numbers = [1 , 3 ,4 ,5 , 23]
 
-function calculateSum(numbers, printSum){
-    console.log(numbers)
-    printSum()
-}
+// function calculateSum(numbers, printSum){
+//     console.log(numbers)
+//     printSum()
+// }
 
-calculateSum(numbers, printSum);
+// calculateSum(numbers, printSum);
 
 
+// function aclean(arr) {
+//     let map = new Map();
+  
+//     for (let word of arr) {
+//       // разбиваем слово на буквы, сортируем и объединяем снова в строку
+//       let sorted = word.toLowerCase().split("").sort().join(""); // (*)
+//       map.set(sorted, word);
+//     }
+  
+//     return Array.from(map.values());
+//   }
+  
+//   let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+  
+//   alert( aclean(arr) );
+
+  let map = new Map();
+
+  map.set("name", "John");
+  
+  let keys = Array.from(map.keys());
+  
+  keys.push("more");
+  
+  alert(keys); // name, more
